25508bf5126c85736631adc2d0c117f7303e7b28
Davis Liu <davisliu2006@gmail.com>
2025-05-20 10:26:59 -0400

    Update system.hpp

--- headers/system.hpp
+++ headers/system.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
+#include <chrono>
 #include <ctime>
 
 // time
 inline double time() {
-    #if defined(__MACOS__)
-    timespec t;
-    clock_gettime(CLOCK_REALTIME, &t);
-    return t.tv_sec+t.tv_nsec*0.000000001;
-    #else
-    return 0;
-    #endif
+    using namespace std::chrono;
+    auto t = steady_clock::now();
+    return duration_cast<microseconds>(t.time_since_epoch()).count()*1e-6;
 }
