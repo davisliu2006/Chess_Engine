f96df697240e01595a5ed20d471a3952512b4783
Davis Liu <davisliu2006@gmail.com>
2025-10-03 02:16:57 -0400

    Openings temp solution

--- Chess.cpp
+++ Chess.cpp
@@ -8,6 +8,7 @@
 #include "headers/system.hpp"
 #include "headers/rng.hpp"
 #include "chess/chess.hpp"
+#include "chess/chess_openings.hpp"
 
 using namespace std;
 using namespace chess;
@@ -26,25 +27,38 @@ int main() {
     cout << "FINISH SETUP" << endl;
 
     stack<CompressedBoard> history;
+    stack<int64_t> opening_history;
+    int64_t opening = 0;
 
-    array<bool,2> is_computer = {true, false};
+    array<bool,2> is_computer = {true, true};
     while (true) {
         history.push(board.compress());
+        opening_history.push(opening);
         cout << "Move #" << history.size() << '\n';
         cout << (white_turn? "White" : "Black") << " to move " << (is_computer[white_turn]? "(computer)" : "(human)") << " \n";
         board.print_board();
         board.print_pcs();
 
         if (is_computer[white_turn]) {
-            move_pair_score_t best_mps = board.get_best_move(4, white_turn);
-            cout << best_mps << '\n';
-            if (best_mps.is_invalid()) {return 0;}
-            auto& [move_pair, move_score] = best_mps;
-            auto& [piece, move] = move_pair;
+            if (history.size() <= 2 && openings::has_opening(opening)) {
+                auto [op1, move_pair] = openings::get_best_opening(opening, board);
+                auto& [piece, move] = move_pair;
 
-            ChessPiece* captpiece = board.grid[move.first][move.second];
-            if (captpiece) {board.rem_piece(*captpiece);}
-            board.move_piece(*piece, move.first, move.second);
+                ChessPiece* captpiece = board.grid[move.first][move.second];
+                if (captpiece) {board.rem_piece(*captpiece);}
+                board.move_piece(*piece, move.first, move.second);
+                opening = (opening << 16) | op1;
+            } else {
+                move_pair_score_t best_mps = board.get_best_move(4, white_turn);
+                cout << best_mps << '\n';
+                if (best_mps.is_invalid()) {return 0;}
+                auto& [move_pair, move_score] = best_mps;
+                auto& [piece, move] = move_pair;
+
+                ChessPiece* captpiece = board.grid[move.first][move.second];
+                if (captpiece) {board.rem_piece(*captpiece);}
+                board.move_piece(*piece, move.first, move.second);
+            }
         } else {
             cout << "Player turn to move <x0, y0, x1, y1>: ";
             int x0 = -1, y0 = -1, x1 = -1, y1 = -1;
@@ -74,12 +88,15 @@ int main() {
             if (undo) {
                 history.pop(); history.pop();
                 ChessBoard::reconstruct(board, history.top()); history.pop();
+                opening_history.pop(); opening_history.pop();
+                opening = opening_history.top(); opening_history.pop();
                 cout << '\n';
                 continue;
             } else {
                 const auto& [piece, move] = *selected;
                 ChessPiece* captpiece = board.grid[move.first][move.second];
                 if (captpiece) {board.rem_piece(*captpiece);}
+                opening = (opening << 16) | (piece->x << 12) | (piece->y << 8) | (move.first << 4) | (move.second);
                 board.move_piece(*piece, move.first, move.second);
             }
         }
--- chess/chess_openings.hpp
+++ chess/chess_openings.hpp
@@ -6,9 +6,12 @@ namespace chess {
     namespace openings {
         extern unordered_map<int64_t, vector<int>> mp;
 
-        inline move_pair_t get_best_opening(int64_t opening, ChessBoard& board) {
+        inline bool has_opening(int64_t opening) {
+            return mp[opening].size() != 0;
+        }
+        inline pair<int64_t,move_pair_t> get_best_opening(int64_t opening, ChessBoard& board) {
             int x = mp[opening][randint(0, mp[opening].size()-1)];
-            return {board.grid[(x>>12)&0xf][(x>>8)&0xf], {(x>>4)&0xf, x&0xf}};
+            return {x, {board.grid[(x>>12)&0xf][(x>>8)&0xf], {(x>>4)&0xf, x&0xf}}};
         };
     }
 }
